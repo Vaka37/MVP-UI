01bb54b47edf69b70df3fdcf5fc8aebb629274dc
Vakil <>
2024-03-13 15:21:27 +0300

    поправил нейминг

--- MVP-UI/MVP-UI/SceneDelegate.swift
+++ MVP-UI/MVP-UI/SceneDelegate.swift
@@ -6,7 +6,7 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     private var applicationCoordinator: ApplicationCoordinator?
-
+    
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
@@ -14,15 +14,8 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         configureSceneDelegate(windowScene: windowScene)
-        let networkService = NetworkService()
-        //TODO: - Запросы для проверки данных
-        networkService.getRecipe { _ in
-        }
-        networkService
-            .getDetail(uri: "http://www.edamam.com/ontologies/edamam.owl#recipe_e074fb5e814ed30309780398e68c2b90") { _ in
-            }
     }
-
+    
     private func configureSceneDelegate(windowScene: UIWindowScene) {
         window = UIWindow(windowScene: windowScene)
         if let window {
